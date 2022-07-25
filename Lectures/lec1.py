dbe49ac7efdd09b1acd569010cd9fbd86da97c8a
Martin Burolla <mburolla@xpanxion.com>
2022-07-25 13:35:09 -0400

    update

--- Lectures/lec1.py
+++ Lectures/lec1.py
@@ -1,16 +1,18 @@
+# type()
+# id()
+# https://www.w3schools.com/python/python_datatypes.asp
+# For loops
+# string slicing : https://www.w3schools.com/python/python_strings_slicing.asp
+# input
+# Array & Dictionaries
+# If
+# While
+# Casting: https://www.w3schools.com/python/python_casting.asp
+# f string
+
+
 def lec1():
     print("Lecture 1")
-    # type()
-    # id()
-    # https://www.w3schools.com/python/python_datatypes.asp
-    # For loops
-    # string slicing : https://www.w3schools.com/python/python_strings_slicing.asp
-    # input
-    # Array & Dictionaries
-    # If
-    # While
-    # Casting: https://www.w3schools.com/python/python_casting.asp
-    # f string
 
 
 if __name__ == '__main__':  # Dunder
