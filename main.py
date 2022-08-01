d4efe996b14986e943dbe0b355a42430b7d2bcde
Michelle Gilyeat <mgilyeat@xpanxion.com>
2022-08-01 17:04:27 -0400

    Ex 13 Frame It

---
+++ main.py
@@ -0,0 +1,15 @@
+#
+# File: main.py
+# Date: 7/3/2022
+# Desc: Update line 7 with your student Id, update line 11 with the exercise you are working on.
+#
+
+from Assignments.stu8.assignments import *
+
+
+def main():
+    ex13()  # <== The exercise you are currently working on.
+
+
+if __name__ == '__main__':
+    main()
